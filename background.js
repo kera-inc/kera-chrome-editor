ff63c3f0ab0c9e4a4ae8d2c4220654923b175efa
Cameron Westland <camwest@gmail.com>
2013-01-29 16:28:39 -0500

    Added initial chrome extension

---
+++ background.js
@@ -0,0 +1,33 @@
+var keraActive = {}
+
+chrome.tabs.onUpdated.addListener(function(tabId) {
+  chrome.pageAction.show(tabId);
+
+  if (!keraActive[tabId]) {
+    deactivate(tabId);
+  } else {
+    activate(tabId);
+  }
+});
+
+chrome.pageAction.onClicked.addListener(function(event) {
+  var tabId = event.id;
+
+  if (!keraActive[tabId]) {
+    keraActive[tabId] = true;
+    activate(tabId);
+  } else {
+    keraActive[tabId] = false;
+    deactivate(tabId);
+  }
+});
+
+function activate(tabId) {
+  chrome.pageAction.setIcon({ tabId: tabId, path: 'active.png' });
+  chrome.tabs.sendMessage(tabId, { method: 'activate' });
+}
+
+function deactivate(tabId) {
+  chrome.pageAction.setIcon({ tabId: tabId, path: 'deactive.png' });
+  chrome.tabs.sendMessage(tabId, { method: 'deactivate' });
+}
