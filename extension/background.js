c6b2c5facc57225c861c645f1f86b05fc8dd0db3
Cameron Westland <camwest@gmail.com>
2013-01-29 19:07:02 -0500

    background script now requests the BAY javascript and injects it into the page

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,61 @@
-function init(chrome) {
+function init(chrome, XMLHttpRequest) {
   var keraActive = {}
 
-  chrome.tabs.onUpdated.addListener(function(tabId) {
-    chrome.pageAction.show(tabId);
+  var BAY_LIB_URL = 'http://localhost:5999/bay.js';
+  var bayLib;
+  var scriptsLoaded = false;
+  var scriptsLoadedCallbacks = [];
 
-    if (!keraActive[tabId]) {
-      deactivate(tabId);
-    } else {
-      activate(tabId);
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', BAY_LIB_URL, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      bayLib = xhr.responseText;
+      scriptsLoaded = true;
+      scriptsLoadedCallbacks.forEach(function(callback) {
+        callback();
+      });
     }
+  }
+
+  xhr.send();
+
+  function injectScript(tabId, callback) {
+    if (scriptsLoaded) {
+      doInject(tabId, callback);
+    } else {
+      scriptsLoadedCallbacks.push(function() {
+        doInject(tabId, callback);
+      });
+    }
+  }
+
+  function doInject(tabId, callback) {
+    chrome.tabs.executeScript(tabId, { code: bayLib  }, function() {
+      callback();
+    });
+  }
+
+  function activate(tabId) {
+    chrome.pageAction.setIcon({ tabId: tabId, path: 'active.png' });
+    chrome.tabs.sendMessage(tabId, { method: 'activate' });
+  }
+
+  function deactivate(tabId) {
+    chrome.pageAction.setIcon({ tabId: tabId, path: 'deactive.png' });
+    chrome.tabs.sendMessage(tabId, { method: 'deactivate' });
+  }
+
+  chrome.tabs.onUpdated.addListener(function(tabId) {
+    injectScript(tabId, function() {
+      chrome.pageAction.show(tabId);
+
+      if (!keraActive[tabId]) {
+        deactivate(tabId);
+      } else {
+        activate(tabId);
+      }
+    });
   });
 
   chrome.pageAction.onClicked.addListener(function(event) {
@@ -22,20 +69,10 @@ function init(chrome) {
       deactivate(tabId);
     }
   });
-
-  function activate(tabId) {
-    chrome.pageAction.setIcon({ tabId: tabId, path: 'active.png' });
-    chrome.tabs.sendMessage(tabId, { method: 'activate' });
-  }
-
-  function deactivate(tabId) {
-    chrome.pageAction.setIcon({ tabId: tabId, path: 'deactive.png' });
-    chrome.tabs.sendMessage(tabId, { method: 'deactivate' });
-  }
 }
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = init;
 } else {
-  init(chrome);
+  init(chrome, XMLHttpRequest);
 }
--- test/background_spec.js
+++ test/background_spec.js
@@ -8,6 +8,40 @@ describe('Background', function() {
     , addEventListenerCallback
     , onUpdatedAddListenerCallback;
 
+  var xhr
+    , xhrInstance
+    , bayLib = 'library';
+
+  function expectActive(tabId) {
+    expect(chrome.pageAction.setIcon.calledWith({ tabId: tabId, path: 'active.png' })).to.equal(true);
+    expect(chrome.tabs.sendMessage.calledWith(tabId, { method: 'activate' })).to.equal(true);
+  }
+
+  function expectDeactive(tabId) {
+    expect(chrome.pageAction.setIcon.calledWith({ tabId: tabId, path: 'deactive.png' })).to.equal(true);
+    expect(chrome.tabs.sendMessage.calledWith(tabId, { method: 'deactivate' })).to.equal(true);
+  }
+
+  function updateTab(tabId) {
+    chrome.pageAction.setIcon = sinon.spy();
+    chrome.pageAction.show = sinon.spy();
+    chrome.tabs.sendMessage = sinon.spy();
+    chrome.tabs.executeScript = function(id, details, callback) {
+      expect(tabId).to.equal(id);
+      expect(details.code).to.equal(bayLib);
+      callback();
+    }
+
+    onUpdatedAddListenerCallback(tabId);
+  }
+
+  function clickPageAction(tabId) {
+    chrome.pageAction.setIcon = sinon.spy();
+    chrome.tabs.sendMessage = sinon.spy();
+
+    addEventListenerCallback({ id: tabId });
+  }
+
   beforeEach(function() {
     chrome = {
       tabs: {
@@ -26,85 +60,82 @@ describe('Background', function() {
       }
     }
 
-    background(chrome);
+    xhr = function() {}
+
+    xhr.prototype = {
+      open: function(method, url, async) {
+        expect(method).to.equal('GET');
+        expect(url).to.equal('http://localhost:5999/bay.js');
+        expect(async).to.equal(true);
+      },
+      send: function() {
+        xhrInstance = this;
+      }
+    }
+
+    background(chrome, xhr);
   });
 
-  function expectActive(tabId) {
-    expect(chrome.pageAction.setIcon.calledWith({ tabId: tabId, path: 'active.png' })).to.equal(true);
-    expect(chrome.tabs.sendMessage.calledWith(tabId, { method: 'activate' })).to.equal(true);
-  }
 
-  function expectDeactive(tabId) {
-    expect(chrome.pageAction.setIcon.calledWith({ tabId: tabId, path: 'deactive.png' })).to.equal(true);
-    expect(chrome.tabs.sendMessage.calledWith(tabId, { method: 'deactivate' })).to.equal(true);
-  }
+  describe('xhr has loaded the library', function() {
+    beforeEach(function() {
+      xhrInstance.responseText = bayLib;
+      xhrInstance.readyState = 4;
+      xhrInstance.onreadystatechange();
+    });
 
-  function updateTab(tabId) {
-    chrome.pageAction.setIcon = sinon.spy();
-    chrome.pageAction.show = sinon.spy();
-    chrome.tabs.sendMessage = sinon.spy();
+    describe('tab updating', function() {
+      describe('when a tab is updated and kera is deactive', function() {
+        var tabId;
 
-    onUpdatedAddListenerCallback(tabId);
-  }
-
-  function clickPageAction(tabId) {
-    chrome.pageAction.setIcon = sinon.spy();
-    chrome.tabs.sendMessage = sinon.spy();
-
-    addEventListenerCallback({ id: tabId });
-  }
-
-  describe('tab updating', function() {
-    describe('when a tab is updated and kera is deactive', function() {
-      var tabId;
-
-      beforeEach(function() {
-        tabId = 1;
-        updateTab(tabId);
-      });
-
-      it('shows the pageAction', function() {
-        expect(chrome.pageAction.show.calledWith(tabId)).to.equal(true);
-      });
-
-      it('sets the tab to deactive', function() {
-        expectDeactive(tabId);
-      });
-
-      describe('when the pageAction is clicked and the page is updated', function() {
         beforeEach(function() {
-          clickPageAction(tabId);
+          tabId = 1;
           updateTab(tabId);
         });
 
-        it('sets the tab to active', function() {
-          expectActive(tabId);
+        it('shows the pageAction', function() {
+          expect(chrome.pageAction.show.calledWith(tabId)).to.equal(true);
+        });
+
+        it('sets the tab to deactive', function() {
+          expectDeactive(tabId);
+        });
+
+        describe('when the pageAction is clicked and the page is updated', function() {
+          beforeEach(function() {
+            clickPageAction(tabId);
+            updateTab(tabId);
+          });
+
+          it('sets the tab to active', function() {
+            expectActive(tabId);
+          });
         });
       });
     });
-  });
 
-  describe('pageAction toggle', function() {
-    describe('when the deactive pageAction is clicked', function() {
-      var tabId;
+    describe('pageAction toggle', function() {
+      describe('when the deactive pageAction is clicked', function() {
+        var tabId;
 
-      beforeEach(function() {
-        tabId = 1;
-
-        clickPageAction(tabId);
-      });
-
-      it('should be active', function() {
-        expectActive(tabId);
-      });
-
-      describe('when the pageAction is clicked second time', function() {
         beforeEach(function() {
+          tabId = 1;
+
           clickPageAction(tabId);
         });
 
-        it('should be deactive', function() {
-          expectDeactive(tabId);
+        it('should be active', function() {
+          expectActive(tabId);
+        });
+
+        describe('when the pageAction is clicked second time', function() {
+          beforeEach(function() {
+            clickPageAction(tabId);
+          });
+
+          it('should be deactive', function() {
+            expectDeactive(tabId);
+          });
         });
       });
     });
