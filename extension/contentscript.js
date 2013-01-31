07b283158a07a7122bace0b73d28408122e554b1
Cameron Westland <camwest@gmail.com>
2013-01-31 10:32:51 -0500

    Content script now responds to login
    Content script can request a login from the background script
    Content script can request a logout from the background script

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,14 +1,30 @@
-function init(chrome) {
+function init(chrome, editor) {
   chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    var editor = require('bay');
+    if (!editor) {
+      editor = require('bay');
+    }
 
     if (request.method == 'activate') {
       editor.show();
+
+      if (request.logged_in == true) {
+        editor.login(request.apiKey);
+      }
     }
 
     if (request.method == 'deactivate') {
       editor.hide();
     }
+
+    editor.on('requestLogin', function() {
+      chrome.extension.sendMessage('requestLogin', function(apiKey) {
+        editor.login(apiKey);
+      });
+    });
+
+    editor.on('logout', function() {
+      chrome.extension.sendMessage('logout');
+    });
   });
 }
 
--- test/contentscript_spec.js
+++ test/contentscript_spec.js
@@ -1,49 +1,138 @@
 describe('ContentScript', function() {
   var sinon = require('sinon')
     , expect = require('expect.js')
-    , contentscript = require('../extension/contentscript.js')
-    , bay = require('bay');
+    , contentscript = require('../extension/contentscript.js');
+
+  var editor;
 
   var chrome
     , onMessageAddListenerCallback;
 
-  function requestMethod(method) {
-    onMessageAddListenerCallback({ method: method });
+  function requestMethod(method, apiKey) {
+    var loggedIn = (apiKey) ? true : false;
+
+    onMessageAddListenerCallback({ method: method, logged_in: loggedIn, apiKey: apiKey });
+  }
+
+  function editorDispatches(eventName) {
+    editor.on.args.forEach(function(arg) {
+      if (arg[0] == eventName) {
+        arg[1]();
+      }
+    });
+  }
+
+  function expectBackgroundMessage(eventName) {
+    var messageFound = false;
+
+    chrome.extension.sendMessage.args.forEach(function(arg) {
+      if (arg[0] == eventName) {
+        messageFound = true;
+      }
+    });
+
+    expect(messageFound).to.equal(true);
+  }
+
+  function backgroundResponds(eventName, results) {
+    chrome.extension.sendMessage.args.forEach(function(arg) {
+      if (arg[0] == eventName) {
+        arg[1](results);
+      }
+    });
   }
 
   beforeEach(function() {
+    editor = {
+      on: sinon.spy(),
+      show: sinon.spy(),
+      login: sinon.spy(),
+      hide: sinon.spy()
+    };
+
     chrome = {
       extension: {
         onMessage: {
           addListener: function(callback) {
             onMessageAddListenerCallback = callback;
           }
-        }
+        },
+
+        sendMessage: sinon.spy()
       }
     }
 
-    contentscript(chrome);
+    contentscript(chrome, editor);
   });
 
-  describe('when activated', function() {
+  describe('when activated and logged in', function() {
+    var apiKey = 'abc123';
     beforeEach(function() {
-      bay.show = sinon.spy();
+      requestMethod('activate', apiKey);
+    });
+
+    it('calls editor.show', function() {
+      expect(editor.show.called).to.equal(true);
+    });
+
+    it('calls editor.login and passes in apiKey', function() {
+      expect(editor.login.calledWith(apiKey)).to.equal(true);
+    });
+  });
+
+  describe('when activated and not logged in', function() {
+    beforeEach(function() {
       requestMethod('activate');
     });
 
     it('calls editor.show', function() {
-      expect(bay.show.called).to.equal(true);
+      expect(editor.show.called).to.equal(true);
+    });
+
+    it('does not call editor.login', function() {
+      expect(editor.login.called).to.equal(false);
     });
   });
 
   describe('when deactivated', function() {
     beforeEach(function() {
-      bay.hide = sinon.spy();
       requestMethod('deactivate');
     });
 
     it('calls editor.hide', function() {
-      expect(bay.hide.called).to.equal(true);
+      expect(editor.hide.called).to.equal(true);
+    });
+  });
+
+  describe('when the editor dispatches "requestLogin"', function() {
+    beforeEach(function() {
+      requestMethod('activate');
+      editorDispatches('requestLogin');
+    });
+
+    it('sends "requestLogin" to the background script', function() {
+      expectBackgroundMessage('requestLogin');
+    });
+
+    describe('when the background script responds with the apiKey', function() {
+      beforeEach(function() {
+        backgroundResponds('requestLogin', 'apiKey');
+      });
+
+      it('calls editor.login and passes in the key', function() {
+        expect(editor.login.calledWith('apiKey')).to.equal(true);
+      });
+    });
+  });
+
+  describe('when the editor dispatches "logout"', function() {
+    beforeEach(function() {
+      requestMethod('activate');
+      editorDispatches('logout');
+    });
+
+    it('sends "logout" to the background script', function() {
+      expectBackgroundMessage('logout');
     });
   });
 });
